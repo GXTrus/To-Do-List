4dfcc46378f4d8b775277f584cf42e33cd7751b6
Alexandr Pahomov <gxtrus@gmail.com>
2020-06-12 21:04:32 +0300

    Stage #3: Deadlines are scary
    Description
    
    Everyday, we’re surrounded by thousands of small distractions, facts, and bits of odd information. When you’re on a deadline, you don’t have time for all this fluff. You need to focus on the task at hand, so you can’t afford to spend hours on Pinterest, chatting at the water cooler with your coworkers, or watching re-runs on TV. At this point in time, the deadline takes precedence.
    
    So let's add the ability to set deadlines to our tasks. Python datetime module will help us to work with dates.
    
    Here are some methods that might help you:
    
    from datetime import datetime, timedelta
    
    datetime.today() # return current date and time.
    datetime.today().date() # current date without time
    datetime.today().time() # current time without date
    
    datetime.strptime(date_string, format) # return a datetime corresponding to date_string, parsed according to format.
    # Format example: '%Y-%m-%d' - '2020-04-24'
    
    today = datetime.today()
    today.day # the day of a current month.
    today.strftime('%b') # the short name of the current month. I.e 'Apr'
    today.weekday() # return the day of the week as an integer, where Monday is 0 and Sunday is 6.
    
    yesterday = today - timedelta(days=1) # a timedelta object represents a duration, the difference between two dates or times.
    day_after_tomorrow = today + timedelta(days=2)
    To select rows from the table with some condition, you can use filter() method that accepts the condition by which to select rows:
    
    from datetime import datetime
    
    today = datetime.today()
    rows = session.query(Table).filter(Table.date == today).all()
    In the code snippet above, we selected all rows from Table where the date column equals today's date.
    
    Objectives
    
    Add the following items to your menu:
    
    Week's tasks: prints all tasks for 7 days from today.
    All tasks: prints all tasks sorted by deadline.
    Now Add task item should ask for the deadline of the task. Users should input the deadline in this format: YYYY-MM-DD.
    
    Also, Today's tasks item should print today's date.
    
    See the format of the output in the example.

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from sqlalchemy import Column, Date, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -32,15 +32,19 @@ class ToDoList:
 
     def work(self):
         while True:
-            print("1) Today's tasks\n2) Add task\n0) Exit")
-            user_input = self.enter_choice(('0', '1', '2'))
-            if user_input == '2':
-                self.add_task()
-            elif user_input == '1':
+            print("1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Add task\n0) Exit")
+            user_input = self.enter_choice(('0', '1', '2', '3', '4'))
+            if user_input == '1':
                 self.today_task()
-            if user_input == '0':
+            elif user_input == '2':
+                self.week_task()
+            elif user_input == '3':
+                self.all_tasks()
+            elif user_input == '4':
+                self.add_task()
+            elif user_input == '0':
                 break
-        print('\nBye!')
+        print('Bye!')
 
     def enter_choice(self, variants):
         while True:
@@ -49,23 +53,48 @@ class ToDoList:
                 break
             else:
                 print('\nerror\n')
+        print()
         return a
 
-    def today_task(self):
-        print('\nToday:')
-        rows = self.session.query(self.TaskTable).filter(self.TaskTable.deadline == self.today).all()
+    def days_tasks(self, day_):
+        rows = self.session.query(self.TaskTable).filter(self.TaskTable.deadline == day_).all()
         if len(rows):
             for a in range(len(rows)):
                 print(f'{a + 1}. {rows[a]}')
-            print()
         else:
-            print('Nothing to do!\n')
+            print('Nothing to do!')
+        print()
+        return True
+
+    def today_task(self):
+        print(f"Today {self.today.day} {self.today.strftime('%b')}:")
+        self.days_tasks(self.today)
+        return True
+
+    def week_task(self):
+        for day_n in range(7):
+            cur_day = self.today + timedelta(days=day_n)
+            week_days = 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split()
+            print(f"{week_days[cur_day.weekday()]} {cur_day.day} {cur_day.strftime('%b')}:")
+            self.days_tasks(cur_day)
+        return True
+
+    def all_tasks(self):
+        rows = self.session.query(self.TaskTable).all()
+        print('All tasks:')
+        if len(rows):
+            for a in range(len(rows)):
+                cur_day = rows[a].deadline
+                print(f"{a + 1}. {rows[a]}. {cur_day.day} {cur_day.strftime('%b')}")
+        else:
+            print('Nothing to do!')
+        print()
         return
 
     def add_task(self):
-        print('\nEnter task')
-        task = input()
-        new_row = self.TaskTable(task=task)
+        task = input('\nEnter task\n')
+        deadline = input('Enter deadline')
+        new_row = self.TaskTable(task=task, deadline=datetime.strptime(deadline, '%Y-%m-%d'))
         self.session.add(new_row)
         self.session.commit()
         print('The task has been added!\n')
