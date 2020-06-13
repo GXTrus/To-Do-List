43d2ad210110964b19b16eecc91752e9c6d38e06
Alexandr Pahomov <gxtrus@gmail.com>
2020-06-13 08:25:24 +0300

    Stage #4: Bye, completed tasks
    Description
    
    Planning is one thing, but when we need to knuckle down and put our plans into action, we tend to push our tasks back further and further until the last minute, or worse — past the established deadline. It happens to the best of us!
    
    In this stage, let's implement the ability to see missed tasks and delete them.
    
    To delete a row from a table, you need to use the delete() method that accepts an object to delete. As you remember, each row is represented by a Python object:
    
    from datetime import datetime
    
    # delete all rows where date column equals today's date
    session.query(Table).filter(Table.date == datetime.today()).delete()
    
    # delete a specific row
    rows = session.query(Table).filter(Table.date < datetime.today()).all()
    specific_row = rows[0] # in case rows is not empty
    session.delete(specific_row)
    
    # don't forget to commit changes
    session.commit()
    To sort selected data, you can use order_by() that accepts a column by which you need to sort data:
    
    # select all rows ordered by the date column
    session.query(Table).order_by(Table.date)
    
    # select all rows where string_fields starts with 'L'. The result is ordered by date column
    session.query(Table).filter(Table.string_field.startswith('L'))).order_by(Table.date)
    Objectives
    
    Add the following items into your menu:
    
    Missed tasks: prints all tasks whose deadline was missed, that is, tasks whose deadline date is earlier than today's date.
    Delete task: deletes the chosen task. Print 'Nothing to delete' if the tasks list is empty.
    Missed tasks should print the tasks ordered by the deadline date.
    
    Delete task should print all the tasks sorted by the deadline date and ask to enter the number of the task to delete.
    
    See in the example what your program should look like.

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -32,16 +32,22 @@ class ToDoList:
 
     def work(self):
         while True:
-            print("1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Add task\n0) Exit")
-            user_input = self.enter_choice(('0', '1', '2', '3', '4'))
+            print(
+                "1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Missed tasks\n5) Add task\n6) Delete task\n0) "
+                "Exit")
+            user_input = self.enter_choice(('0', '1', '2', '3', '4', '5', '6'))
             if user_input == '1':
-                self.today_task()
+                self.today_tasks()
             elif user_input == '2':
-                self.week_task()
+                self.weeks_tasks()
             elif user_input == '3':
                 self.all_tasks()
             elif user_input == '4':
+                self.missed_tasks()
+            elif user_input == '5':
                 self.add_task()
+            elif user_input == '6':
+                self.delete_task()
             elif user_input == '0':
                 break
         print('Bye!')
@@ -66,12 +72,14 @@ class ToDoList:
         print()
         return True
 
-    def today_task(self):
+    # 1 Today's tasks
+    def today_tasks(self):
         print(f"Today {self.today.day} {self.today.strftime('%b')}:")
         self.days_tasks(self.today)
         return True
 
-    def week_task(self):
+    # 2 Week's tasks
+    def weeks_tasks(self):
         for day_n in range(7):
             cur_day = self.today + timedelta(days=day_n)
             week_days = 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday'.split()
@@ -79,6 +87,7 @@ class ToDoList:
             self.days_tasks(cur_day)
         return True
 
+    # 3 All tasks
     def all_tasks(self):
         rows = self.session.query(self.TaskTable).all()
         print('All tasks:')
@@ -89,16 +98,46 @@ class ToDoList:
         else:
             print('Nothing to do!')
         print()
-        return
+        return True
 
+    # 4 Missed tasks
+    def missed_tasks(self):
+        rows = self.session.query(self.TaskTable).filter(self.TaskTable.deadline < self.today).all()
+        print('Missed tasks:')
+        if len(rows):
+            for a in range(len(rows)):
+                cur_day = rows[a].deadline
+                print(f"{a + 1}. {rows[a]}. {cur_day.day} {cur_day.strftime('%b')}")
+        else:
+            print('Nothing is missed!')
+        print()
+        return True
+
+    # 5 Add task
     def add_task(self):
         task = input('\nEnter task\n')
-        deadline = input('Enter deadline')
+        deadline = input('Enter deadline\n')
         new_row = self.TaskTable(task=task, deadline=datetime.strptime(deadline, '%Y-%m-%d'))
         self.session.add(new_row)
         self.session.commit()
         print('The task has been added!\n')
-        return
+        return True
+
+    # 6 Delete task
+    def delete_task(self):
+        print('\nChose the number of the task you want to delete')
+        self.session.query(self.TaskTable).order_by(self.TaskTable.deadline)
+        rows = self.session.query(self.TaskTable).all()
+        if len(rows):
+            for a in range(len(rows)):
+                cur_day = rows[a].deadline
+                print(f"{a + 1}. {rows[a]}. {cur_day.day} {cur_day.strftime('%b')}")
+        x = int(input())
+        specific_row = rows[x - 1]
+        self.session.delete(specific_row)
+        self.session.commit()
+        print('The task has been deleted!\n')
+        return True
 
 
 if __name__ == '__main__':
